6f2613095c5097dc8b106f218e82c21dd3e304f6
Andrew <144896298+sawandrew93@users.noreply.github.com>
2026-05-18 11:15:39 +0630

    Remove SLES 15 SP6 dependency checks and repos
    Removed dependency checking and repository addition for SLES 15 SP6.

--- install_v1.1.sh
+++ install_v1.1.sh
@@ -290,46 +290,6 @@ user_exists=$(
   | tr -d '"'
 )
 
-
-
-#dependency checking
-# Add SLES 15 SP6 base product and modules repos from RMT mirror
-
-BASE_URL="http://repo.vanguardmm.com/repo/SUSE"
-
-# Base Product
-zypper ar -f $BASE_URL/Products/SLE-Product-SLES/15-SP6/x86_64/product/ SLES15-SP6-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Product-SLES/15-SP6/x86_64/update/ SLES15-SP6-Updates
-
-# Base Module
-zypper ar -f $BASE_URL/Products/SLE-Module-Basesystem/15-SP6/x86_64/product/ SLE-Base-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-Basesystem/15-SP6/x86_64/update/ SLE-Base-Updates
-
-# Desktop Applications
-zypper ar -f $BASE_URL/Products/SLE-Module-Desktop-Applications/15-SP6/x86_64/product/ SLE-Desktop-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-Desktop-Applications/15-SP6/x86_64/update/ SLE-Desktop-Updates
-
-# Development Tools
-zypper ar -f $BASE_URL/Products/SLE-Module-Development-Tools/15-SP6/x86_64/product/ SLE-DevTools-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-Development-Tools/15-SP6/x86_64/update/ SLE-DevTools-Updates
-
-# Legacy
-zypper ar -f $BASE_URL/Products/SLE-Module-Legacy/15-SP6/x86_64/product/ SLE-Legacy-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-Legacy/15-SP6/x86_64/update/ SLE-Legacy-Updates
-
-# Python 3
-zypper ar -f $BASE_URL/Products/SLE-Module-Python3/15-SP6/x86_64/product/ SLE-Python3-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-Python3/15-SP6/x86_64/update/ SLE-Python3-Updates
-
-# SAP Business One
-zypper ar -f $BASE_URL/Products/SLE-Module-SAP-Business-One/15-SP6/x86_64/product/ SLE-SAPB1-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-SAP-Business-One/15-SP6/x86_64/update/ SLE-SAPB1-Updates
-
-# Server Applications
-zypper ar -f $BASE_URL/Products/SLE-Module-Server-Applications/15-SP6/x86_64/product/ SLE-ServerApps-Pool
-zypper ar -f $BASE_URL/Updates/SLE-Module-Server-Applications/15-SP6/x86_64/update/ SLE-ServerApps-Updates
-
-
 echo "Checking and installing required packages..." | tee -a "$LOGFILE"
 
 PACKAGES="jq libatomic1 rpm-build xmlstarlet python3-pyOpenSSL bc glibc-i18ndata \
